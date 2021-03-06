2e401b5a4cd61d75ad412cecb869b810f352308f
Vera Chikhareva <vera.chepovaya@mail.ru>
2021-03-06 21:48:26 +0300

    add test

---
+++ src/test/java/ru/netology/SeleniumTest.java
@@ -0,0 +1,48 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SeleniumTest {
+    private WebDriver driver;
+
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Chikhareva\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldSubmitRequest() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        WebElement form = driver.findElement(By.cssSelector("[data-test-id]"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Татьяна");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[data-test-id=submit]")).click();
+        String text = driver.findElement(By.className("alert-success")).getText();
+        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+    }
+}
+
