8b70317fc65f821192a339764ffd27e6507ee86e
Artem <romanov-artyom@bk.ru>
2021-03-19 19:04:44 +0300

    Update SeleniumTest.java

--- src/test/java/ru/netology/SeleniumTest.java
+++ src/test/java/ru/netology/SeleniumTest.java
@@ -26,11 +26,11 @@ public class SeleniumTest {
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
 
